da6bd2740521ada2a727001bf78769f8f15dae0d
Dima <dimasalabay0711@gmail.com>
2023-11-05 14:46:01 +0200

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+function createMarkup(arr) {
+	return arr
+		.map(
+			({ preview, description, original }) =>
+				`<li class="gallery__item">
+					<a class="gallery__link" href="${original}">
+						 <img class="gallery__image" src="${preview}" alt="${description}" />
+					</a>
+				</li>`
+		)
+		.join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionPosition: "bottom",
+	captionDelay: 250,
+});
